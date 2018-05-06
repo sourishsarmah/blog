491041f006e33b5cc24b94180d131ebb65d447b3
Sourish Sarmah <sourishsarmah@gmail.com>
2018-05-06 13:10:04 +0530

    User Profile Added

--- app/routes.py
+++ app/routes.py
@@ -44,7 +44,7 @@ def login():
         login_user(user, remember = form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('user')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
